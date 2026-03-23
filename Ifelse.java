ae3a2cfc9e4d4e6246f5eb96652147b0498a2668
nehaldevlab <mdnehalsince2003@gmail.com>
2026-03-23 23:18:43 +0530

    moved files and removed Java-Foundation folder

---
+++ Ifelse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ifelse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Age - ");
+        int age=sc.nextInt();
+        if(age>=18){
+          System.out.println("Adult");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+    }
+    
+}
